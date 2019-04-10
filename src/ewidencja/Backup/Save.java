e11d7636aa0877ad86432e2b54def2543aa4e2cb
SzymonFed <szymonfederkiewicz@gmail.com>
2019-04-10 21:17:04 +0200

    TODO: all

---
+++ src/ewidencja/Backup/Save.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ewidencja.Backup;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Szymon
+ */
+public class Save {
+    
+
+}
